4ea6d14f8315242c79417501931b4928ec011e58
kareemkrion <kareemkrion@gmail.com>
2022-10-16 15:53:00 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,8 +9,8 @@ int main(void)
 	int num;
 	char le;
 
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) = '0');
+	for (num = 0; num < 16; num++)
+		putchar((num % 16) = '0');
 
 	for (le = 'a'; <= 'f'; le++)
 		putchar(le);
