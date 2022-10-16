d5f953893fe014f287d853d1d24093e9776d963e
kareemkrion <kareemkrion@gmail.com>
2022-10-16 16:09:48 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all combination of single digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 58; n < 68; n++)
+	{
+		putchar (n);
+	}
+		if (n != 67)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+
+		return (0);
+}
