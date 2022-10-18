a02ac49b95c639d99dcf20f7572dbf4806055a87
kareemkrion <kareemkrion@gmail.com>
2022-10-18 13:12:39 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,14 @@
  */
 int main(void)
 {
-	putchar("_putchar");
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
 	return (0);
 }
