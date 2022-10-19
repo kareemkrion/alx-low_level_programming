99edbd0d8216daba44f23a333aa605945300a9ad
kareemkrion <kareemkrion@gmail.com>
2022-10-18 18:09:10 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ */
+void print_alphabet_x10(void);
+{
+	int n;
+	char bt;
+
+	for (n = '0'; n <= '9' n++)
+	{
+		for (bt = 'a'; bt <= 'z'; bt++)
+			_putchar(bt);
+		_putchar('\n');
+	}
+}
