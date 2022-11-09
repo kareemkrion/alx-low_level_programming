3d4de1564397d1cc093eb0060959d2ac85b65b50
kareemkrion <kareemkrion@gmail.com>
2022-11-09 05:35:53 -0800

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name followed by a new line
+ * @argc: number of command line arguments
+ * @argv: arraythat contains the program command line arguments
+ * Return: 0 - Sucess
+ */
+int main( int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
